32b1d1dc3a739a6d52a7be580e17b9e4dba5bf4d
jonataspinto <jonatastdb_jp@hotmail.com>
2023-09-20 22:34:51 -0300

    feat(app/repositories): implements ContactsRepository

---
+++ src/app/repositories/ContactsRepository.js
@@ -0,0 +1,41 @@
+const { v4: uuid } = require('uuid');
+
+let contacts = [
+  {
+    id: uuid(),
+    name: 'Mateus',
+    email:'mateus@mail.com',
+    phone: '12345678',
+    category_id: uuid(),
+  },
+  {
+    id: uuid(),
+    name: 'Jose',
+    email:'jose@mail.com',
+    phone: '12345678',
+    category_id: uuid(),
+  }
+]
+
+class ContactsRepository {
+  findAll(){
+    return new Promise((resolve) => {
+      resolve(contacts)
+    });
+  }
+
+  findById(id){
+    return new Promise((resolve) => {
+      resolve(contacts.find(contact => contact.id === id))
+    });
+  }
+
+  delete(id){
+    return new Promise((resolve) => {
+      contacts = contacts.filter(contact => contact.id !== id)
+      resolve();
+    });
+  }
+}
+
+module.exports = new ContactsRepository();
