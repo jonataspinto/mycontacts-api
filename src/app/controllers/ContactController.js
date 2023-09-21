2128746c4830e6d2edab78907617dbc0a8ba9536
jonataspinto <jonatastdb_jp@hotmail.com>
2023-09-20 22:35:17 -0300

    feat(app/controllers): implements ContactController

---
+++ src/app/controllers/ContactController.js
@@ -0,0 +1,44 @@
+const ContactsRepository = require("../repositories/ContactsRepository");
+
+class ContactController {
+  async index(request, response) {
+    const contacts = await ContactsRepository.findAll();
+    response.json(contacts)
+  }
+
+  async show(request, response) {
+    const { id } = request.params;
+
+    const contact = await ContactsRepository.findById(id)
+
+    if(!contact) {
+      return response.status(404).json({ error: 'user not found'})
+    }
+
+    response.json(contact)
+  }
+
+  store(request, response) {
+    // criar novo registro
+  }
+
+  update(request, response) {
+    // atualizar o registro especificado
+  }
+
+  async delete(request, response) {
+    const { id } = request.params;
+
+    const contact = await ContactsRepository.findById(id);
+
+    if(!contact) {
+      return response.status(404).json({ error: 'user not found'});
+    }
+
+    await ContactsRepository.delete(id);
+
+    response.sendStatus(204);
+  }
+}
+
+module.exports = new ContactController();
