6dd466764ed23f5efa21a7c1b504eca9e2285192
jonataspinto <jonatastdb_jp@hotmail.com>
2023-09-25 15:04:24 -0300

    feat(app/repository/category):  implements methods - findById - update - delete

--- src/app/repositories/CategoriesRepository.js
+++ src/app/repositories/CategoriesRepository.js
@@ -12,12 +12,42 @@ class CategoriesRepository {
 
   async create({ name }) {
     const [row] = await DATABASE.query(`
-      INSERT INTO categories(name)
-      VALUES($1)
-      RETURNING *
+    INSERT INTO categories(name)
+    VALUES($1)
+    RETURNING *
     `, [name]);
 
     return row;
+  }
+
+  async findById(id) {
+    const [rows] = await DATABASE.query(`
+      SELECT * FROM categories
+      WHERE id = $1
+      ORDER by name
+    `, [id]);
+
+    return rows;
+  }
+
+  async update(id, { name }) {
+    const [row] = await DATABASE.query(`
+      UPDATE categories
+      SET name = $2
+      WHERE id = $1
+      RETURNING *
+    `, [id, name]);
+
+    return row;
+  }
+
+  async delete(id) {
+    const deleteOperation = await DATABASE.query(`
+      DELETE from categories
+      WHERE id = $1
+    `, [id]);
+
+    return deleteOperation;
   }
 }
 
