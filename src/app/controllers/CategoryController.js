a2188141dbfe6921b671fa15a547ec8cc5f371f3
jonataspinto <jonatastdb_jp@hotmail.com>
2023-09-25 15:04:58 -0300

    feat(app/controllers/category):  implements methods - show - update - delete

--- src/app/controllers/CategoryController.js
+++ src/app/controllers/CategoryController.js
@@ -18,6 +18,41 @@ class CategoryController {
 
     response.json(category);
   }
+
+  async show(request, response) {
+    const { id } = request.params;
+
+    const category = await CategoriesRepository.findById(id);
+
+    response.json(category);
+  }
+
+  async update(request, response) {
+    const { id } = request.params;
+    const { name } = request.body;
+
+    const categoryExists = await CategoriesRepository.findById(id);
+
+    if (!categoryExists) {
+      return response.status(404).json({ error: 'category not found' });
+    }
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+
+    const category = await CategoriesRepository.update(id, { name });
+
+    response.json(category);
+  }
+
+  async delete(request, response) {
+    const { id } = request.params;
+
+    await CategoriesRepository.delete(id);
+
+    response.send(204);
+  }
 }
 
 module.exports = new CategoryController();
